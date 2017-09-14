ff3f908695f1ccec31e550e33b7f40325589acb1
jmunozal <jmunozal@gmail.com>
2017-09-14 12:25:06 +0200

    initial commit: ViewController and two operations

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Jesús Muñoz on 14/09/2017.
+//  Copyright © 2017 Jesús Muñoz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsInTheMiddleOfTyping = false
+    
+    @IBAction func touchDigit(_ sender: UIButton) {
+        // underbar: not external touchDigig(someButton). Not used for 2-> n argument, only (ocasionally) for the first
+        let digit = sender.currentTitle!
+        if (userIsInTheMiddleOfTyping) {
+            let textCurrentlyInDisplay = display.text!
+            display.text = textCurrentlyInDisplay + digit
+        }
+        else {
+            display.text = digit
+            userIsInTheMiddleOfTyping = true
+        }
+    }
+
+    // Double: type
+    // two names: 1st: external (used by the caller. Mandatory for the caller.)
+    // 2nd: internal (inside of this method)
+    func drawHorizontalLine(from startX: Double, to endX: Double, using color: UIColor) -> String {
+        return ""
+    }
+    
+    var displayValue: Double {
+        get {
+            return Double(display.text!)!
+        }
+        set {
+            display.text = String(newValue)
+        }
+    }
+    
+    @IBAction func performOperation(_ sender: UIButton) {
+        userIsInTheMiddleOfTyping = false
+        if let matemeticalSymbol = sender.currentTitle {
+            switch matemeticalSymbol {
+            case "π":
+                // conversion:
+                displayValue = Double.pi
+            case "√":
+                // square root:
+                displayValue = sqrt(displayValue)
+            default:
+                break
+            }
+        }
+    }
+    
+}
+
